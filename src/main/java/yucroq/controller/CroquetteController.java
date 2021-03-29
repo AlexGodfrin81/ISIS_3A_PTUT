35c17804ac343c3cc0ea63a426d3869c9375e4c6
Alex <Neptuna1998@gmail.com>
2021-03-29 14:54:50 +0200

    Supression animal/conseils.html

---
+++ src/main/java/yucroq/controller/CroquetteController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yucroq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import yucroq.dao.CroquetteRepository;
+
+/**
+ *
+ * @author ALEX
+ */
+public class CroquetteController {
+    
+    @Autowired
+    private CroquetteRepository dao;
+    
+    /**
+     * Affiche toutes les catégories dans la base
+     *
+     * @param model pour transmettre les informations à la vue
+     * @return le nom de la vue à afficher ('afficheGTableaux.html')
+     */
+    @GetMapping(path = "show")
+    public String afficheToutesLesCroquettes(Model model) {
+        model.addAttribute("croquette", dao.findAll());
+        return "afficheCroquette";
+    }
+    
+}
