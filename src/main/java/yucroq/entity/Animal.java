5dae812ccd862f4957ba040454d1f0755a689e77
agathe.sutarik <agathe@sutarik.com>
2021-03-29 10:19:48 +0200

    Ajout du calcul du besoin énergétique dans Animal

--- src/main/java/yucroq/entity/Animal.java
+++ src/main/java/yucroq/entity/Animal.java
@@ -89,4 +89,24 @@ public class Animal {
         return this.mesPoids.get(0).getPoids_kg()*(100/(100+(this.getNec()-5)));
     }
     
+    /**
+     * Calcule le besoin énergétique BE d'un animal.
+     * @return le besoin énergétique, en fonction du coefficient K et du besoin énergétique à l'entretien
+     */
+    public double calcul_BE(){
+        // si l'espèce est un chat, alors la puissance sera de 0.67, pour un chien 0.75
+        // attention, à changer si on rajoute une espèce (switch case)
+        double BE= Math.pow((double)(poids_ideal()), (Espece.CHAT==this.espece)?0.67:0.75); 
+        float k1=this.maRace.getK1();
+        float k2=this.monActivite.getK2();
+        float k3 =this.monStadePhysio.getK3();
+        // A changer au besoin
+        // coeff déterminé en fonction état de santé de l'animal
+        // on considère tous les animaux en bonne santé donc k4=1
+        float k4=1;
+        // coeff déterminé en fonction de la température de l'environnement
+        // peu utilisé et difficile à évaluer --> k5=1
+        float k5=1;
+        return BE*k1*k2*k3*k4*k5;
+    }
 }
