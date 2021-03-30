6ca6b48756e68a3c7d7ba1d2558f88ac923136b8
agathe.sutarik <agathe@sutarik.com>
2021-03-30 16:45:01 +0200

    Suppression des id dans data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,28 @@
 -- Les données qui seront initialisées automatiquement quand on lance l'application
 -- cf. application.properties
-INSERT INTO Activite(id_activite, nom, k2) VALUES
-    (1, 'Peu actif', 0.8),
-    (2, 'Actif', 1);
+INSERT INTO Activite(nom, k2) VALUES
+    ('Peu actif', 0.8),
+    ('Actif', 1);
 
-INSERT INTO Animal(id_animal, nom, espece, est_Male, date_naiss, taille_cm, nec) VALUES
-    (1, 'Hilda', 'CHIEN', false, '2012-03-01', 100, 3),
-    (2, 'Papouille', 'CHAT', true, '2005-06-05', 45, 7);
+INSERT INTO Animal(nom, espece, est_Male, date_naiss, taille_cm, nec) VALUES
+    ('Hilda', 'CHIEN', false, '2012-03-01', 100, 3),
+    ('Papouille', 'CHAT', true, '2005-06-05', 45, 7);
 
-INSERT INTO Croquette(id_croq, nom, marque, espece, humidite, proteines_brutes, matieres_grasses, cellulose, matieres_minerales, calcium, phosphore) VALUES
-    (1, 'ProPlan', 'Purina', 'CHIEN', 8, 15, 12, 5, 5, 1.2, 1.1);
+INSERT INTO Croquette(nom, marque, espece, humidite, proteines_brutes, matieres_grasses, cellulose, matieres_minerales, calcium, phosphore) VALUES
+    ('ProPlan', 'Purina', 'CHIEN', 8, 15, 12, 5, 5, 1.2, 1.1);
 
-INSERT INTO Proprietaire(id_proprio, nom, prenom, tel) VALUES 
-    (1, 'Garriga', 'Léandre', '0600000000'),
-    (2, 'Sutarik', 'Agathe', '0660606060');
+INSERT INTO Proprietaire(nom, prenom, tel) VALUES 
+    ('Garriga', 'Léandre', '0600000000'),
+    ('Sutarik', 'Agathe', '0660606060');
 
-INSERT INTO Race(id_race, nom, k1, espece) VALUES
-    (1, 'Berger Blanc Suisse', 1, 'CHIEN'),
-    (2, 'Caniche nain', 1, 'CHIEN');
+INSERT INTO Race(nom, k1, espece) VALUES
+    ('Berger Blanc Suisse', 1, 'CHIEN'),
+    ('Caniche nain', 1, 'CHIEN');
 
-INSERT INTO StadePhysiologique(id_stade, nom, k3) VALUES
-    (1, 'Entier', 1),
-    (2, 'Stérilisé', 0.8);
+INSERT INTO StadePhysiologique(nom, k3) VALUES
+    ('Entier', 1),
+    ('Stérilisé', 0.8);
 
-INSERT INTO SuiviPoids(id_suivi, date, poids_kg) VALUES
-    (1, '2021-03-01', 42),
-    (2, '2021-03-08', 41.5);
+INSERT INTO SuiviPoids(date, poids_kg) VALUES
+    ('2021-03-01', 42),
+    ('2021-03-08', 41.5);
