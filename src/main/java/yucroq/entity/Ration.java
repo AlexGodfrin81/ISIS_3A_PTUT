82f253070a5d42252cd9f8c848e4f4285e488806
iJoff <joffrey.desplanque@etud.univ-jfc.fr>
2021-04-07 10:41:37 +0200

    Ajout de l'entité Ration

---
+++ src/main/java/yucroq/entity/Ration.java
@@ -0,0 +1,52 @@
+package yucroq.entity;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *
+ * @author joffreyviemondesplanque
+ */
+
+@Entity
+@Getter @Setter @RequiredArgsConstructor @NoArgsConstructor @ToString
+public class Ration {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_ration;
+    
+    @Column
+    @NonNull
+    private float quantite;
+    
+    @Column
+    @NonNull
+    private float quantite_ideale;
+    
+    @Column
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date_debut;
+    
+    @Column
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date_fin;
+}
