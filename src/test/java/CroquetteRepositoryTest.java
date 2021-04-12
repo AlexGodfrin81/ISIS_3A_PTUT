aac893f72b62e99373263a788b28c0060c94da65
LeandreGarriga <>
2021-04-12 11:07:48 +0200

    Test( c'est cassé)

--- src/test/java/CroquetteRepositoryTest.java
+++ src/test/java/CroquetteRepositoryTest.java
@@ -1,31 +1,27 @@
-
-import lombok.extern.log4j.Log4j2;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import yucroq.dao.CroquetteRepository;
-import yucroq.entity.Croquette;
-
-
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-/**
- *
- * @author ALEX
- */
+
+import lombok.extern.log4j.Log4j2;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import yucroq.dao.CroquetteRepository;
+import yucroq.entity.Croquette;
+import org.junit.jupiter.api.Test;
+
+
 @Log4j2 // Génère le 'logger' pour afficher les messages de trace
 @DataJpaTest
-public class Test {
+public class CroquetteRepositoryTest {
     @Autowired
     private CroquetteRepository croquetteDAO;
     
-    //@Test
+    @Test
     public void testMethodeDensiteEnergetique(){
         Croquette croq = croquetteDAO.findById(1).orElseThrow();
         log.info("On calcule la densite énergetique de la croquette {} en java", croq.getNom());
@@ -33,6 +29,5 @@ public class Test {
                 "La densite energetique de ces croquettes est de 34.544f" );
         
     }
-    
     
 }
