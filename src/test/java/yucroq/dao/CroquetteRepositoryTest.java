7eb14bb4f1cad601520f59c3b9b4f940a7c97cfb
LeandreGarriga <>
2021-04-12 11:22:36 +0200

    Test( c'est cassé mais c'est mieux)

--- src/test/java/yucroq/dao/CroquetteRepositoryTest.java
+++ src/test/java/yucroq/dao/CroquetteRepositoryTest.java
@@ -1,3 +1,5 @@
+package yucroq.dao;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +11,12 @@ import lombok.extern.log4j.Log4j2;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import org.springframework.test.context.jdbc.Sql;
 import yucroq.dao.CroquetteRepository;
 import yucroq.entity.Croquette;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
 @Log4j2 // Génère le 'logger' pour afficher les messages de trace
