eb605829d08e115c53c5f544f85c1ae10567113d
agathe.sutarik <agathe@sutarik.com>
2021-04-15 09:11:06 +0200

    Finition de la connexion todo ; rediriger apres s'etre connecté

--- src/main/java/yucroq/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package yucroq.dao;
-
-import yucroq.entity.Proprietaire;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Proprietaire, Long> {
-    Proprietaire findByUsername(String name);
-}
