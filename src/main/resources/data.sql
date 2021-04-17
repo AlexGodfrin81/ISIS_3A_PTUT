f6ab32b7da7a4ccf6d49ca0283bb394434536160
iJoff <joffrey.desplanque@etud.univ-jfc.fr>
2021-04-17 16:17:25 +0200

    Ajout des recommandations (en mode barbare : le code peut être amélioré)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,7 +23,7 @@ INSERT INTO Stade_Physiologique(id_stade, nom, k3) VALUES
     (2, 'Stérilisé', 0.8);
 
 INSERT INTO Animal(id_animal, nom, espece, est_Male, date_naiss, taille_cm, nec, id_proprio, id_race, id_activite, id_stade) VALUES
-    (1, 'Hilda', 'CHIEN', false, '2012-03-01', 100, 6, 3, 1, 2, 2),
+    (1, 'Hilda', 'CHIEN', false, '2012-03-01', 100, 6, 3, 1, 1, 2),
     (2, 'Papouille', 'CHIEN', true, '2005-06-05', 45, 7, 1, 2, 2, 1),
     (3, 'Fidji', 'CHAT', true, '2010-02-28', 20, 3, 3, 4, 2, 2);
 
@@ -34,5 +34,5 @@ INSERT INTO Suivi_Poids(id_suivi, date_suivi, poids_kg, id_animal) VALUES
 
 INSERT INTO Ration(id_ration, quantite, date_debut, date_fin, id_animal, id_croq) VALUES
     (1, 200, '2012-03-01', '2012-03-03', 1, 1),
-    (3, 293, '2012-03-03', null, 1, 1),
+    (3, 400, '2012-03-03', null, 1, 1),
     (2, 180, '2012-03-03', '2012-03-05', 2, 1);
