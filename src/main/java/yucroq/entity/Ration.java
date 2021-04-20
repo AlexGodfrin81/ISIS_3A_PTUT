9a5449eb323e336ece25c86c0daf18d4b494a693
iJoff <joffrey.desplanque@etud.univ-jfc.fr>
2021-04-20 10:39:53 +0200

    Retour aux dates YYYY-MM-DD

--- src/main/java/yucroq/entity/Ration.java
+++ src/main/java/yucroq/entity/Ration.java
@@ -41,11 +41,11 @@ public class Ration {
     private float quantite;
     
     @Column
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date_debut;
     
     @Column
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date_fin;
     
      //-== Mapping ==-
